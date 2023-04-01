f47347da8b691b3233579510df50e05f56a8df22
Firli Ilhami <firli.ilhami@gmail.com>
2023-04-01 14:27:14 +0700

    add profile page and avatars

--- app/models.py
+++ app/models.py
@@ -1,8 +1,15 @@
-from app import db
+from app import db, login
+from werkzeug.security import (
+    generate_password_hash,
+    check_password_hash
+)
+from flask_login import UserMixin
+from hashlib import md5
+
 from datetime import datetime
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -13,6 +20,18 @@ class User(db.Model):
     # p = Post(body='my first post!', author=u), notice the author=u, not user_id=u
     # this is abstraction layer
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(128))
+    last_seen = db.Column(db.DateTime, default= datetime.utcnow)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -26,4 +45,10 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
-        return '<Post {}>'.format(self.body)
+        return f'<Post {self.body}>'
+
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
