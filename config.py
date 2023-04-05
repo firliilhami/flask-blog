04b9d57889a5437ddcb258b04912e3a36e23f5dd
Firli Ilhami <firli.ilhami@gmail.com>
2023-04-05 22:41:33 +0700

    using .env and adjust unittest

--- config.py
+++ config.py
@@ -1,21 +1,23 @@
 import os
+from dotenv import load_dotenv
 
 basedir = os.path.dirname(__file__)
+load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'sandbox.smtp.mailtrap.io'
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 2525)
-    MAIL_USE_TLS = True # os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT'))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USE_SSL = False
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or '9689877281d769'
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or '71e2047fc941ff'
-    ADMINS = ['firli.pad@gmail.com']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = [os.environ.get('ADMINS')]
     TESTING = False
     MAIL_SUPPRESS_SEND = False
 
